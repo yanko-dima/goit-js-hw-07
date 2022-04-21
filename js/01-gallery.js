771e358e2c4052ae0f4534950db106526c34540e
Yanko Dima <yanko.dmitriy@gmail.com>
2022-04-22 00:06:41 +0300

    Gallery markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.addEventListener('click', onClickPreviewGallery)
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+            </a>
+        </div>
+        `;
+    })
+    .join('');
+}
+
+function onClickPreviewGallery (evt) {
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    console.log(evt.target.nodeName);
+}
