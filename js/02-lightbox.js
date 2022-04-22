0d1d6eb94734ea9fc04bbaf7027b708a014bccca
Yanko Dima <yanko.dmitriy@gmail.com>
2022-04-23 02:44:22 +0300

    Task2 Markup + Library

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener('click', onPreviewGalleryClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+          <li>
+          <a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+          </li>
+          `;
+    })
+    .join('');
+}
+
+function onPreviewGalleryClick(evt) {
+  evt.preventDefault();
+
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const previewImg = evt.target;
+  const originalImg = previewImg.dataset.source;
+
+  console.log(previewImg);
+  
+  // openModalImg(previewImg, originalImg);
+  // closeModalImg();
+}
